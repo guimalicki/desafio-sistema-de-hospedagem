6d5f98dfea21ad563337aed127d74d7e16d47cc9
Guilherme Malicki <guimalicki2002@gmail.com>
2025-08-18 20:58:54 -0300

    feat: Criação da classe Suite

---
+++ Models/Suite.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sistema_de_hospedagem.Models
+{
+    public class Suite
+    {
+        //Atributos
+        private string _tipoSuite;
+        private int _capacidade;
+        private decimal _valorDiaria;
+
+        //Getters e Setters
+        public string TipoSuite
+        {
+            get { return _tipoSuite; }
+            set { _tipoSuite = value; }
+        }
+        public int Capacidade
+        {
+            get { return _capacidade; }
+            set { _capacidade = value; }
+        }
+        public decimal ValorDiaria
+        {
+            get { return _valorDiaria; }
+            set { _valorDiaria = value; }
+        }
+
+        //Construtor
+        public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
+        {
+            _tipoSuite = tipoSuite;
+            _capacidade = capacidade;
+            _valorDiaria = valorDiaria;
+        }
+    }
+}
