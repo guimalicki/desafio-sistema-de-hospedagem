72eea6420e2c159f78bb796cf4d6d4eeaf23ab96
Guilherme Malicki <guimalicki2002@gmail.com>
2025-08-18 21:05:21 -0300

    feat: Criação da classe Reserva

---
+++ Models/Reserva.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sistema_de_hospedagem.Models
+{
+    public class Reserva
+    {
+        //Atributos
+        private List<Pessoa> _hospedes = new List<Pessoa>();
+        private Suite _suite;
+        private int _diasReservados;
+
+        //Getters e Setters
+        //Não criamos setters para a lista de hóspedes, pois não queremos que ela seja modificada diretamente.
+        public List<Pessoa> Hospedes
+        {
+            get { return _hospedes; }
+        }
+        public Suite Suite
+        {
+            get { return _suite; }
+            set { _suite = value; }
+        }
+        public int DiasReservados
+        {
+            get { return _diasReservados; }
+            set { _diasReservados = value; }
+        }
+
+        //Construtor
+        public Reserva(Suite suite, int diasReservadas)
+        {
+            _suite = suite;
+            _diasReservados = diasReservadas;
+        }
+
+        //Métodos
+        //Cadastro da suite
+        public void CadastrarSuite(Suite suite)
+        {
+            _suite = suite;
+        }
+
+        //Cadastrar hóspedes
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            //Verifica se o número de hóspedes não excede a capacidade da suíte
+            if (hospedes.Count > _suite.Capacidade)
+            {
+                throw new Exception("Número de hóspedes excede a capacidade da suíte.");
+            }
+            else
+            {
+                //Adiciona os hóspedes à lista
+                _hospedes.AddRange(hospedes);
+            }
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            //Retorna a quantidade de hóspedes cadastrados
+            return _hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            return _suite.ValorDiaria * _diasReservados;
+        }
+    }
+}
